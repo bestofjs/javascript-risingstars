098e2f0b1467c89c18036e90b11113a091cf95d9
michaelrambeau <michael@medmain.com>
2020-12-30 17:59:22 +0900

    Exclude learning resource projects from the "all" categories

--- src/utils/processProjectData.js
+++ src/utils/processProjectData.js
@@ -1,79 +1,82 @@
-const slugify = require('./slugify')
+const slugify = require("./slugify");
 
-const processProject = item => {
+const processProject = (item) => {
   return {
     ...item,
     repository: `https://github.com/${item.full_name}`,
-    slug: slugify(item.name)
-  }
-}
+    slug: slugify(item.name),
+  };
+};
 
-const sortBy = fn => (a, b) => fn(b) - fn(a)
-const sortByYearlyDelta = sortBy(project => project.delta)
+const sortBy = (fn) => (a, b) => fn(b) - fn(a);
+const sortByYearlyDelta = sortBy((project) => project.delta);
 
 function getSortedProjects(entities) {
-  return Object.values(entities)
-    .slice()
-    .sort(sortByYearlyDelta)
+  return Object.values(entities).slice().sort(sortByYearlyDelta);
 }
 
 function isMatchingTag(category, project) {
-  const tags = category.tags || [category.key]
-  return tags.some(tag => project.tags.includes(tag))
+  const tags = category.tags || [category.key];
+  return tags.some((tag) => project.tags.includes(tag));
 }
 
 function filterByTag(sortedProjects, entities, category) {
-  const { key, excluded, count } = category
-  const isMatching = project =>
-    key === 'all' || isMatchingTag(category, project)
-  const isExcluded = project => excluded && excluded.includes(project.slug)
+  const { key, excluded, count } = category;
+  const isMatching = (project) => {
+    if (key === "all") {
+      return !project.tags.includes("learning");
+    }
+    return isMatchingTag(category, project);
+  };
+  const isExcluded = (project) => excluded && excluded.includes(project.slug);
   return sortedProjects.filter(
-    project => isMatching(project) && !isExcluded(project)
-  )
+    (project) => isMatching(project) && !isExcluded(project)
+  );
 }
 
 function getProjectsByTag(sortedProjects, entities, categories) {
   return categories.reduce(
     (result, category) =>
       Object.assign({}, result, {
-        [category.key]: filterByTag(sortedProjects, entities, category)
+        [category.key]: filterByTag(sortedProjects, entities, category),
       }),
     {}
-  )
+  );
 }
 
 function getProjectsBySlug(payload) {
   const projectsBySlug = payload.projects
     .map(processProject)
     .reduce((acc, project) => {
-      return { ...acc, [project.slug]: project }
-    }, {})
-  return projectsBySlug
+      return { ...acc, [project.slug]: project };
+    }, {});
+  return projectsBySlug;
 }
 
 function getTopProjects(projectsByTag, categories) {
-  return categories.reduce(
-    (acc, category) => {
-      const categoryTopProjects = projectsByTag[category.key].slice(0, category.count + 1)
-      return [...acc, ...categoryTopProjects]
-    }, []
-  )
+  return categories.reduce((acc, category) => {
+    const categoryTopProjects = projectsByTag[category.key].slice(
+      0,
+      category.count + 1
+    );
+    return [...acc, ...categoryTopProjects];
+  }, []);
 }
 
 function getMiscProjects(projectsByTag, categories) {
-   const topProjects = getTopProjects(projectsByTag, categories)
-   return projectsByTag.all.filter(p => !topProjects.includes(p))
+  const topProjects = getTopProjects(projectsByTag, categories);
+  return projectsByTag.all.filter((p) => !topProjects.includes(p));
 }
 
 function processProjectData(projectData, categories) {
-  const entities = getProjectsBySlug(projectData)
-  const sorted = getSortedProjects(entities)
-  const projectsByTag = getProjectsByTag(sorted, entities, categories)
-  projectsByTag.misc = getMiscProjects(projectsByTag, categories)
+  const entities = getProjectsBySlug(projectData);
+  const sorted = getSortedProjects(entities);
+  const projectsByTag = getProjectsByTag(sorted, entities, categories);
+  projectsByTag.misc = getMiscProjects(projectsByTag, categories);
   return {
     projectsByTag,
-    entities
-  }
+    entities,
+  };
 }
 
-module.exports = processProjectData
+module.exports = processProjectData;
