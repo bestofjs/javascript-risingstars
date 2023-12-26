d8fa91a76215ca2a15414c79a376181feadd4945
michaelrambeau <mikeair@gmail.com>
2023-12-26 17:17:42 +0900

    Remove useless test

--- app/test/server-component.tsx
+++
@@ -1,8 +0,0 @@
-export async function MyServerComponent() {
-  await wait()
-  return <div>From the server!</div>;
-}
-
-async function wait() {
-  return new Promise((resolve) => setTimeout(resolve, 100));
-}
