0beb02689a0c10cd63e6e3527286af137b8dcf3c
michaelrambeau <mikeair@gmail.com>
2023-12-27 10:30:48 +0900

    Add metadata

--- app/[year]/[language]/layout.tsx
+++ app/[year]/[language]/layout.tsx
@@ -22,6 +22,11 @@ export default function PageLayout({
         )}
       >
         {children}
+        <script
+          data-goatcounter="https://risingstars.goatcounter.com/count"
+          async
+          src="//gc.zgo.at/count.js"
+        ></script>
       </body>
     </html>
   );
@@ -33,73 +38,36 @@ export async function generateMetadata({ params }) {
 
   const title = t("page.title");
   const description = t("page.description");
+  const rootURL = process.env.NEXT_PUBLIC_ROOT_URL || ""; // env. variable setup from Vercel dashboard
+  const pageURL = `${rootURL}/${year}/${language}`;
+  const imageURL = `${rootURL}/img/${year}/${language}/rising-stars.png`; // Twitter needs an absolute URL?
+
   const metaData: Metadata = {
     title,
     description,
+    icons: {
+      icon: "/favicon.ico",
+      shortcut: "/favicon-16x16.png",
+      apple: "/apple-touch-icon.png",
+    },
+    openGraph: {
+      title,
+      description,
+      type: "article",
+      url: pageURL,
+      publishedTime: `${year}-01-02T00:00:00Z`,
+      images: [
+        {
+          url: imageURL,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [imageURL],
+    },
   };
   return metaData;
 }
-
-// TODO implement the other metadata fields
-
-// Format for Next.js app router:
-
-// export const metadata: Metadata = {
-//   title: {
-//     default: APP_DISPLAY_NAME,
-//     template: `${APP_DISPLAY_NAME} • %s`,
-//   },
-//   description: APP_DESCRIPTION,
-//   icons: {
-//     icon: "/favicon.ico",
-//     shortcut: "/favicon-16x16.png",
-//     apple: "/apple-touch-icon.png",
-//   },
-//   metadataBase: getMetadataRootURL(),
-//   openGraph: {
-//     url: APP_CANONICAL_URL,
-//     title: APP_DISPLAY_NAME,
-//     description: APP_DESCRIPTION,
-//   },
-// };
-
-// Data to migrate, from the previous Head component:
-
-// export default async function Head({ params }: Props) {
-//   const { language, year } = params;
-//   const { t } = await useTranslation(language, year);
-
-//   const title = t("page.title");
-//   const description = t("page.description");
-
-//   const rootURL = process.env.NEXT_PUBLIC_ROOT_URL || ""; // env. variable setup from Vercel dashboard
-//   const pageURL = `${rootURL}/${year}/${language}`;
-//   const imageURL = `${rootURL}/img/${year}/${language}/rising-stars.png`; // Twitter needs an absolute URL?
-
-//   return (
-//     <>
-//       <title>{title}</title>
-//       <meta content="width=device-width, initial-scale=1" name="viewport" />
-//       <link rel="icon" href="/favicon.ico" />
-//       <meta property="og:title" content={title} />
-//       <meta name="twitter:title" content={title} />
-//       <meta name="description" content={description} />
-//       <meta property="og:description" content={description} />
-//       <meta property="og:type" content="article" />
-//       <meta property="og:url" content={pageURL} />
-//       <meta property="og:image" content={imageURL} />
-//       <meta name="twitter:image:src" content={imageURL} />
-//       <meta name="twitter:card" content="summary_large_image" />
-//       <link
-//         precedence="default"
-//         href="https://fonts.googleapis.com/css?family=Space+Mono:400,400i|Roboto+Slab:300,400,700"
-//         rel="stylesheet"
-//       />
-//       <script
-//         data-goatcounter="https://risingstars.goatcounter.com/count"
-//         async
-//         src="//gc.zgo.at/count.js"
-//       ></script>
-//     </>
-//   );
-// }
