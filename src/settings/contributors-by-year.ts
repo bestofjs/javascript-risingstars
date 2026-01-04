a9d088c3b95325785ca2de6b07d05e801f4823d4
michaelrambeau <mikeair@gmail.com>
2026-01-04 12:30:02 +0900

    Add OG image for Spanish edition

--- src/settings/contributors-by-year.ts
+++ src/settings/contributors-by-year.ts
@@ -86,7 +86,9 @@ export const byYear: { [year: string]: unknown } = {
   },
   "2025": {
     authors: ["michaelrambeau"],
-    translators: {},
+    translators: {
+      es: ["jvalen"],
+    },
   },
 };
 
